62d4537ef1b68dbdc8f86ff6cb51107f64318a79
17luba <146106312+17luba@users.noreply.github.com>
2025-03-28 11:52:47 +0100

    Configuration du système de tir et aussi avec un effet de tremblement de la camera lors du tir

--- Assets/Scripts/ShootScript.cs
+++ Assets/Scripts/ShootScript.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using EZCameraShake;
 
 public class ShootScript : MonoBehaviour
 {
 
     public Transform Gun;
 
+    public Animator GunAnimator;
+
     Vector2 direction;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject Bullet;
+    public float BulletSpeed;
+    public Transform ShootPoint;
+
+    public float fireRate;
+
+    float ReadyForNextShot;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -19,10 +31,28 @@ public class ShootScript : MonoBehaviour
         direction = mousePos - (Vector2)Gun.position;
 
         FaceMouse();
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time > ReadyForNextShot)
+            {
+                ReadyForNextShot = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
     }
 
     void FaceMouse()
     {
         Gun.transform.right = direction;
+    }
+
+    void Shoot()
+    {
+        GameObject BulletIns = Instantiate(Bullet, ShootPoint.position, ShootPoint.rotation);
+        BulletIns.GetComponent<Rigidbody2D>().AddForce(BulletIns.transform.right * BulletSpeed);
+        GunAnimator.SetTrigger("Shoot");
+        CameraShaker.Instance.ShakeOnce(1.2f, 0.8f, 0.1f, 0.15f);
+        Destroy(BulletIns, 1.5f);
     }
 }
